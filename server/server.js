47552df7e62d57f312fc01128d522895dc992f11
Albert Akopyan <alakopyan.exceedteam@gmail.com>
2021-04-14 16:37:45 +0300

    change

--- server/server.js
+++ server/server.js
@@ -56,6 +56,7 @@ app.get("/todos/", (req, res) => {
 require("./app/routes/todo.routes")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 4000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });

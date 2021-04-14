172edd0f68d64802909815bc548ed4b8f7901be6
Albert Akopyan <alakopyan.exceedteam@gmail.com>
2021-04-14 15:57:40 +0300

    change api.js

--- src/services/API.js
+++ src/services/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:4000/api/todos/";
+const API_URL = "";
 
 async function createToDo(title) {
   const { data: newTodo } = await axios.post(API_URL, {
